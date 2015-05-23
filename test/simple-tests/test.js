5752082dbeaea61c83d76c26c7882989ae63798a
Andrey Listochkin <listochkin@outlook.com>
2015-05-23 16:45:22 +0300

    async test with callbacks

--- test/simple-tests/test.js
+++ test/simple-tests/test.js
@@ -1,7 +1,20 @@
-const { assert, expect } = require('chai');
+const { assert, expect } = require('chai'),
+  request = require('request'),
+  jsdom = require('jsdom');
 
 describe('Simple tests', () => {
   it('Should pass', () => {
     assert(1 == '1');
+  });
+
+  it('Should do an async test using callbacks', (done) => {
+    request.get('https://nodejs.org/dist/', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      jsdom.env(body, (error, window) => {
+        const links = window.document.querySelectorAll('a');
+        expect(links.length).to.equal(256);
+        done();
+      });
+    });
   });
 });
